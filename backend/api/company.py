8e1120bb719e80821b8deca8370bb04d253db2b8
henry <henry@yescnc.co.kr>
2023-01-25 00:27:20 +0900

    company 중간 머지

--- backend/api/company.py
+++ backend/api/company.py
@@ -1,6 +1,8 @@
 from flask_restx import Namespace, Resource, fields
 from flask import request
-from backend.company.services import create_company, update_company, delete_company
+from backend.company.services import create_company, update_company, delete_company, read_all_company, search_company, \
+    rank_company
+
 api = Namespace("company", description="Company API")
 
 company_fields = api.model(
@@ -12,7 +14,8 @@ company_fields = api.model(
                 "pay": fields.String, "courl": fields.String,
                 "logo": fields.String, "logo_url": fields.String,
                 "resign": fields.String,"form": fields.String,
-                "bookmarkcnt": fields.Integer, "readcnt": fields.Integer
+                "bookmarkcnt": fields.Integer, "readcnt": fields.Integer,
+                "uno": fields.Integer
                 }
 )
 
@@ -20,6 +23,25 @@ company_delete_fields = api.model(
     "CompanyDelete", {
                     "cname": fields.String,
                     "uno": fields.Integer
+                }
+)
+
+company_read_fields = api.model(
+    "CompanyRead", {
+                    "uno": fields.Integer
+                }
+)
+
+search_company_fields = api.model(
+    "SearchCompany", {
+                    "searchData": fields.String
+                }
+)
+
+rank_company_fields = api.model(
+    "RankCompany", {
+                    "f_all": fields.Integer,
+                    "type": fields.String
                 }
 )
 
@@ -40,12 +62,28 @@ class DeleteCompany(Resource):
     def post(self):
         """Company Delete"""
         return delete_company(request.get_json())
-
+@api.doc(body=company_read_fields)
 class ReadAllCompany(Resource):
-    def get(self):
+    def post(self):
         """Get All Company"""
+        return read_all_company(request.get_json())
+
+@api.doc(body=search_company_fields)
+class SearchCompany(Resource):
+    def post(self):
+        """Search Company"""
+        return search_company(request.get_json())
+
+@api.doc(body=rank_company_fields)
+class RankCompany(Resource):
+    def post(self):
+        """Search Company"""
+        return rank_company(request.get_json())
 
 
 api.add_resource(CreateCompany, "/create")
 api.add_resource(UpdateCompany, "/update")
 api.add_resource(DeleteCompany, "/delete")
+api.add_resource(ReadAllCompany, "/readall")
+api.add_resource(SearchCompany, "/search")
+api.add_resource(RankCompany, "/rank")
