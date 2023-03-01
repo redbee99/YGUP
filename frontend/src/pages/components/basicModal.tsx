0ab206bc8a484087182553a0fef2702ff2bb71ad
sehun <sehun910124@naver.com>
2023-03-01 13:01:58 +0900

    모달수정

--- frontend/src/pages/components/basicModal.tsx
+++ frontend/src/pages/components/basicModal.tsx
@@ -8,12 +8,48 @@ import { set } from '../../reducers/modalReducer'
 import { BaseUrl } from '../../util/axiosApi';
 import React from "react";
 import { useNavigate } from 'react-router-dom';
+import {useSpring, animated} from 'react-spring';
+import {useDrag} from 'react-use-gesture';
+import { useState, useEffect } from 'react';
+
 type Props = {
     content: string,
     _cashe: string
 }
 
 const BasicModal: React.FC<Props> = ({content, _cashe }:Props) => {
+
+    const goUserinfo = () => {
+        navigate('/userinfo')
+    };
+
+    const [pass, setPwValue] = React.useState('');
+    const pwChange = (newValue: string) => {
+        setPwValue(newValue);
+    };
+
+    const inputChange: (event: React.ChangeEvent<HTMLInputElement>) => void = (event) => {
+        const { value, name } = event.target;
+        setnewpw
+        ({
+        ...newpw,[name]: value
+        })
+    }
+
+    const [newpw, setnewpw] = useState({
+        pw1: '',
+        pw2: ''
+    })
+
+    /*modal drag*/
+    const modalDrag = useSpring({x:0, y:0});
+
+    const bindModaldrag = useDrag((params)=>{
+        modalDrag.x.set(params.offset[0]);
+        modalDrag.y.set(params.offset[1]);
+        });
+    /*modal Drag*/
+        
     const dispatch = useDispatch();
 
     const [cashe,] = React.useState(_cashe);
@@ -171,12 +207,27 @@ const BasicModal: React.FC<Props> = ({content, _cashe }:Props) => {
             .catch(function(error) {
                 alert('실패되었습니다.')
             })
-          }
+          }else if(content == "비밀번호 변경"){
+                const url = BaseUrl + "/user/pwupdate"
+                axios.post(url, {
+                    headers:
+                    {
+                        "Content-Type": "application/json"
+                    },
+                    body: { id: id, pw: pw, newpw: newpw, newpw_chk: newpw.pw2}
+                })
+                .then(function(response) {
+                    alert('비밀번호 변경 성공')
+                    goUserinfo()         
+                })
+                .catch(function(error) {
+                    alert('비밀번호를 확인해주세요')
+                })
+             }
         }
 
 
     const DynamicContent = () => {
-
         if(content == "아이디"){
             return  <Box sx={{ mt:10, mb:15 }} >
                 <Typography sx={{ fontSize: 20, fontWeight:'bold', mb:5}}>
@@ -281,58 +332,31 @@ const BasicModal: React.FC<Props> = ({content, _cashe }:Props) => {
                     </Box>
                 </Box>
             )
-        } else if (content == "기업 삭제") {
+        }else if(content == "비밀번호 변경"){
             return (
-              <div className='Modal'>
-                <div className='Modal1' onClick={(e) => e.stopPropagation()}>
-                <Box sx={{ display: 'flex',
-                        textAlign:'center', 
-                        backgroundColor:'#ffffff', 
-                        flexDirection: 'column', 
-                        mt:5, mb:1 
-                }}>
-                    <button id="modalCloseBtn" onClick={() => dispatch(set({state:'off', cashe1:'', cashe2:''})) }>
-                          ✖
-                    </button>
-                   <Typography sx={{ fontSize: 20, fontWeight:'bold', mb:5}}>
-                                정말 삭제 하시겠습니까?
-                   </Typography>
-                   <hr className='login-idsearch_result-underline'/>
-            <Stack direction="row" spacing={2} sx={{ margin:'auto' }} >
-                <Button variant="contained"  
-                        size="small"
-                        onClick={confirm}
-                        sx={{ color:'#ffff', 
-                                backgroundColor: '#26A689', 
-                                borderColor:'#434343'
-                            }} 
-                >
-                    확인
-                </Button>
-                <Button
-                    onClick={ () => dispatch(set({state:'off', cashe1:'', cashe2:''})) }
-                    variant="contained"  
-                    size="small" 
-                    sx={{ color:'#ffff', 
-                          backgroundColor: '#26A689',
-                          borderColor:'#434343'
-                        }} 
-                >
-                    취소
-                </Button>
-            </Stack>
+                <Box sx={{ mt:5, mb:5 }}>
+                    <Typography sx={{ fontSize: 20, fontWeight:'bold' }} color="#434343" gutterBottom>
+                            {content}
+                        </Typography>
+                    <Stack direction='column' spacing={3}>
+                        <TextField id="update-pw" type='password' label="현재 비밀번호" onChange={(newValue) => pwChange(newValue.target.value)} variant="outlined" size="small" margin="normal"/>
+                        <TextField id="update-newpw" type='password' label="새 비밀번호" onChange={inputChange}  size="small" margin="normal" name="pw1" />
+                        <TextField id="update-newpw_chk" type='password' label="새 비밀번호 확인" onChange={inputChange} variant="outlined" size="small" margin="normal" name="pw2"/>
+                    </Stack>
                 </Box>
-                </div>
-              </div>
-            )
-        }
+            );
+            }
         else {
             return(<div/>)
         }
     }
 
-    if(content != "회원 삭제" && content != "기업 삭제" ){
+    if(content != "회원 삭제" ){
         return (
+        <animated.div {...bindModaldrag()} style={{
+            x: modalDrag.x,
+            y: modalDrag.y
+            }}>
             <Box sx={{ display: 'flex',
                         position:'relative', 
                         width:400, 
@@ -372,6 +396,7 @@ const BasicModal: React.FC<Props> = ({content, _cashe }:Props) => {
                 </Button>
             </Stack>
             </Box>
+            </animated.div>
         );
     }
     else{
